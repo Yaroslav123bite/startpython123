adec9c8ae275e3f16f3a264f6d8475d68306dfc3
Yaroslav123bite <smirnov20088@gmail.com>
2025-04-21 16:57:10 +0300

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,48 @@
+# #Callable
+# a =[1, 1.0,'hello']
+# help(a)
+# print(a)
+# b = input('Введи число')
+# len(a)
+# min(a)
+# max(a)
+# type(a)
+# #Простые
+# a=1
+# a=1.0
+# a=1 + 1j
+# a= True
+# a= None
+# #Составные
+# a='hello'
+# a=[1, 1.0, 'hello']
+# a=(1, 1.0, 'hello')
+# a={1, 1.0, 'hello'}
+# a=frozenset({1, 1.0, 'hello'})
+# a={'a': 1, 'b': 2}
+# a=b'hello'
+# a=bytearray(b'hello')
+# #Динамическая типизация
+# a=1
+# a='новое значение переменной a'
+# #Неявная типизация
+# some_var:int=1
+# other_var:str='Hello World'
+# #Сильная типизация
+# 5 + 1.1
+# 5 + 'str'
+
+
+a = 1, 2, 3
+b = str(a)
+print(b)
+print(type(b))
+
+a = 1, 5.5, "hello"
+b = list(a)
+print(b)
+print(type(b))
+
+
+
+
